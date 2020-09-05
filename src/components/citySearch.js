e29f803a1ef99d135281506b8d11e4d3d95c53e2
Christopher Ramos <25787207+Cramos711@users.noreply.github.com>
2020-09-05 02:10:40 -0400

    added google Places API call

--- src/components/citySearch.js
+++ src/components/citySearch.js
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 import { Formik, Field, Form } from 'formik'
+import axios from 'axios'
 
 const CitySearch = () => {
+  const [city, setCity] = useState('orlando')
+  const [newCity, setNewCity] = useState('')
+
+  const params = {
+    params: {
+      address: city,
+      key: process.env.REACT_APP_GOOGLE_API_KEY,
+    },
+  }
+
+  axios
+    .get('https://maps.googleapis.com/maps/api/geocode/json', params)
+    .then(async (response) => {
+      let formattedAddress = response.data.results[0].formatted_address
+      await setNewCity(formattedAddress)
+      console.log(formattedAddress)
+      // var lat = response.data.results[0].geometry.location.lat
+      // var lng = response.data.results[0].geometry.location.lng
+    })
+    .catch((err) => console.log(err))
+
   return (
     <>
       <Formik
@@ -9,8 +32,8 @@ const CitySearch = () => {
           city: '',
         }}
         onSubmit={async (value) => {
-          await new Promise((r) => setTimeout(r, 500))
-          console.log(value.city)
+          await new Promise((res) => setTimeout(res, 500))
+          setCity(value.city)
         }}
       >
         <Form>
@@ -19,6 +42,10 @@ const CitySearch = () => {
           <button type="submit">Submit</button>
         </Form>
       </Formik>
+      <p>You searched for {newCity}</p>
+      {/* <p>
+        That is here -> {lat} {lng}
+      </p> */}
     </>
   )
 }
